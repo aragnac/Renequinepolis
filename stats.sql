9f6ad8304f540fb6236ca62cfc601bc4cf63f84e
Floryan Simar <floryansimar@gmail.com>
2017-09-29 23:01:02 +0200

    Modification des champs en number

--- stats.sql
+++ stats.sql
@@ -2,16 +2,16 @@ DROP TABLE stats;
 CREATE TABLE stats (
   champ         VARCHAR2(15) PRIMARY KEY,
   datatype      VARCHAR2(8),
-  mini          INT,
-  maxi          INT,
-  moyenne       INT,
-  medianne      INT,
-  ecartType     INT,
-  nbVal         INT,
-  nbNull        INT,
-  nbZero        INT,
-  quant95       INT,
-  valdistinctes INT
+  mini          NUMBER,
+  maxi          NUMBER,
+  moyenne       NUMBER,
+  medianne      NUMBER,
+  ecartType     NUMBER,
+  nbVal         NUMBER,
+  nbNull        NUMBER,
+  nbZero        NUMBER,
+  quant95       NUMBER,
+  valdistinctes NUMBER
 );
 SELECT *
 FROM stats
